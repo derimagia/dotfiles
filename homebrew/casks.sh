1ca101253caa524e0a2d3c587df8107868186df3
Dave Wikoff <dwikoff@mindgrub.com>
2015-04-04 13:27:43 -0400

    More casks

--- homebrew/casks.sh
+++ homebrew/casks.sh
@@ -3,6 +3,41 @@
 # Homebrew
 #
 # This installs casks
+brew install caskroom/cask/brew-cask
+
+brew cask install caskroom/homebrew-versions/java6
 
 brew cask install launchrocket
 brew cask install 1password
+brew cask install phpstorm
+brew cask install pycharm-ce
+brew cask install intellij-idea-ce
+brew cask install android-studio
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install sourcetree
+brew cask install dropbox
+brew cask install adium
+brew cask install google-chrome
+brew cask install firefox
+brew cask install iterm2
+brew cask install slack
+brew cask install sublime-text
+brew cask install dash
+brew cask install evernote
+brew cask install alfred
+brew cask install bettertouchtool
+brew cask install rightzoom
+brew cask install charles
+brew cask install atom
+brew cask install mou
+brew cask install mumble
+brew cask install steam
+brew cask install macfusion
+brew cask install xtrafinder
+brew cask install vlc
+brew cask install diffmerge
+brew cask install sequel-pro
+brew cask install psequel
+brew cask install virtualbox
+brew cask install teamviewer
--- ruby/install.sh
+++ ruby/install.sh
@@ -1,9 +1,3 @@
-if test ! $(which rvm)
-then
-  echo "  Installing RVM for you."
-  curl -sSL https://get.rvm.io | bash -s stable --ruby
-fi
-
 # Bundler
 gem install bundler
 
