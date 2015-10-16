8b5c4562ae53d2336c25d039cbdc6dfa56b0bd42
Dave Wikoff <dwikoff@mindgrub.com>
2015-10-16 00:38:06 -0400

    Using brew-file for brew / cask dependencies

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -1,2 +1,7 @@
 # Install all casks to /Applications
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_BREWFILE=~/.dotfiles/homebrew/Brewfile
+
+if [ -f $(brew --prefix)/etc/brew-wrap ];then
+  source $(brew --prefix)/etc/brew-wrap
+fi
--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,7 +8,7 @@
 # Check for Homebrew
 if test ! $(which brew)
 then
-  echo "  Installing Homebrew for you."
+  echo "Installing Homebrew for you."
 
   # Install the correct homebrew for each OS type
   if test "$(uname)" = "Darwin"
@@ -18,96 +18,12 @@ then
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
-  
+
+  brew install rcmdnk/file/brew-file
 fi
 
-# Upgrade any already-installed formulae
-brew upgrade
-
-brew tap homebrew/dupes
-brew tap homebrew/php
-brew tap homebrew/versions
-
-# Install GNU core utilities (those that come with OS X are outdated)
-brew install coreutils # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install moreutils # Install some other useful utilities like `sponge`
-brew install findutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
-brew install gnu-sed --default-names # Install GNU `sed`, overwriting the built-in `sed`
-brew install grc
-brew install spark
-
-# Install wget with IRI support
-brew install wget --enable-iri
-
-# Install other useful binaries
-brew install ack
-#install exiv2
-brew install pv
-brew install rename
-brew install tree
-brew install p7zip
-#install zopfli
-brew install cowsay
-brew install fortune
-brew install htop
-brew install tmux
-
-# Searching / Analyzing
-brew install jq
-brew install pup
-brew install the_silver_searcher
-
-# Security / Network
-brew install nmap
-brew install mtr
-brew install aircrack-ng
-brew install netcat
-brew install httpie
-
-# Languages
-brew install ruby
-brew install node
-brew install python
-
-# GIT / VS
-brew install git
-brew install git-extras
-brew install git-flow
-brew install bfg
-brew install icdiff
-
-# Media 
-brew install imagemagick --with-webp
-brew install ffmpeg
-brew install webkit2png
-brew install pngcheck
-
-# Javascript / Web
-brew install spidermonkey
-brew install v8
-brew install phantomjs
-brew install sassc
-
-brew install lastpass-cli --with-pinentry
-brew install android-platform-tools
-
-# Databases
-brew install mysql
-brew install mongodb
-brew install postgresql
-brew install redis
-
-# PHP
-brew install php53
-brew install php55
-brew install php57
-brew install wp-cli
-brew install drush
-brew install composer
-
-$ZSH/homebrew/casks.sh
-
-# Remove outdated versions from the cellar
-brew cleanup
+# Update using brew-file
+brew file update
+brew file cask_upgrade -C
 
 exit 0
