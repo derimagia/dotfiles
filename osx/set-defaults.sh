17526d1888b68e1f542824aec837cc4d7bd6e551
Dave Wikoff <dgsims@gmail.com>
2016-01-17 13:22:32 -0500

    Adding bitbar

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -35,3 +35,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Bitbar
+defaults write com.matryer.BitBar pluginsDirectory ~/.dotfiles/bitbar/enabled
