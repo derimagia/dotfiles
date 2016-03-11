dff4d388e92153cbc60536cf49054824ad59bead
Dave Wikoff <dgsims@gmail.com>
2016-03-10 23:37:59 -0500

    Adding Hopper

--- system/path.zsh
+++ system/path.zsh
@@ -1,7 +1,7 @@
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
-# ZSH Directory, ./bin Directory
-PATH="./bin:$ZSH/bin:$PATH"
+# ZSH Directory, ./bin Directory, gopath
+PATH="./bin:$ZSH/bin:$PATH:$GOPATH/bin"
 
 # Coreutils
 PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
