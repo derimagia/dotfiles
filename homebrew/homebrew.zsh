b90e3fefc4c95c8af29c69e1d139fd6af5693acb
Dave Wikoff <dgsims@gmail.com>
2016-03-15 01:43:23 -0400

    Making gitconfig source out, Cleaning up, adding autosuggestions

--- homebrew/homebrew.zsh
+++ homebrew/homebrew.zsh
@@ -1,6 +1,6 @@
 # Install all casks to /Applications
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-export HOMEBREW_BREWFILE=~/.dotfiles/homebrew/Brewfile
+export HOMEBREW_BREWFILE=$ZSH/homebrew/Brewfile
 
 if [ -f $HOMEBREW_PREFIX/etc/brew-wrap ];then
   source $HOMEBREW_PREFIX/etc/brew-wrap
--- system/aliases.zsh
+++ system/aliases.zsh
@@ -2,3 +2,5 @@ alias reload!='. ~/.zshrc'
 alias st='stree'
 alias androidem='emulator @Nexus_6P_API_23'
 alias androidemproxy='emulator @Nexus_6P_API_23 -http-proxy http://`ipconfig getifaddr en0`:8888'
+alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
