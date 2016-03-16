0cf491ecce5a3c9fb18e6e132c6bc1d7b423b7f5
Dave Wikoff <dgsims@gmail.com>
2016-03-16 00:02:19 -0400

    Cleaning up / Improving Speeds

---
+++ bin/cli-plugin-example-master/README.md
@@ -0,0 +1,6 @@
+# Terminus Plugin Example
+
+A simple plugin for Terminus-CLI to demonstrate how to add new commands.
+
+Learn more about Terminus and Terminus Plugins at:
+[https://github.com/pantheon-systems/cli/wiki/Plugins](https://github.com/pantheon-systems/cli/wiki/Plugins)
--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -35,6 +35,3 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Bitbar
-defaults write com.matryer.BitBar pluginsDirectory ~/.dotfiles/bitbar/enabled
