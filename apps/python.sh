5cde2fc7835463917352621ef697ef35b1bf1cb0
Dave Wikoff <dgsims@gmail.com>
2016-04-26 01:24:37 -0400

    Updating install file for ubuntu

--- apps/python.sh
+++ apps/python.sh
@@ -1,5 +1,2 @@
 #!/usr/bin/env bash
 pip install virtualenv
-pip-app livestreamer
-pip-app pygments
-pip-app cider
--- apps/ruby.sh
+++ apps/ruby.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 gem install bundler
-gem install bropages
 gem install compass
 gem install cocoapods
 gem install apiaryio
