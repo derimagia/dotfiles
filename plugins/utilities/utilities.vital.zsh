6338a72d71f9fae29cdfe5fb6f84accd2c7b4230
Dave Wikoff <dgsims@gmail.com>
2016-06-18 01:09:41 -0400

    Typo

--- plugins/utilities/utilities.vital.zsh
+++ plugins/utilities/utilities.vital.zsh
@@ -8,4 +8,4 @@ is_linux() { [[ $SHELL_PLATFORM == 'linux' || $SHELL_PLATFORM == 'bsd' ]]; }
 is_osx() { [[ $SHELL_PLATFORM == 'osx' ]]; }
 is_login_shell() { [[ $SHLVL == 1 ]]; }
 is_interactive_shell() { [ ! -z "$PS1" ]; }
-is_ssh_running() { [ ! -z "$SSH_CONECTION" ]; }
+is_ssh_running() { [ ! -z "$SSH_CONNECTION" ]; }
