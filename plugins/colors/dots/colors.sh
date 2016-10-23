f60d3785ffcdf2cdbdcf27ebc92526ae9aeabae3
Dave Wikoff <dgsims@gmail.com>
2016-10-23 17:56:27 -0400

    Fixing link script

---
+++ plugins/colors/dots/colors.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+[[ ! -d /usr/local/etc ]] || exit 34
+
+symlink-files $DOTFILES/plugins/colors/etc/grc.conf /usr/local/etc/
--- plugins/utilities/dots/link.zsh
+++ plugins/utilities/dots/link.zsh
@@ -5,11 +5,13 @@ DOTFILES=${DOTFILES-$HOME/.dotfiles/}
 
 # finds all .dotfiles in this folder
 excluded=($(git --git-dir=$DOTFILES/.git check-ignore *(D)) .git .gitignore .DS_Store .editorconfig)
-excluded=($DOTFILES/$^excluded[@])
+excluded=($DOTFILES/**/$^excluded[@](N))
 
 dotfiles_all=($DOTFILES/plugins/*/.*)
 files=(${dotfiles_all:|excluded})
-dotfiles_source=(${dotfiles_all:|excluded})
+
+# Exclude excluded and add back global gitignore
+dotfiles_source=($DOTFILES/plugins/git/.gitignore ${dotfiles_all:|excluded})
 
 print -l $dotfiles_source
 symlink-files "$dotfiles_source" "$HOME"
