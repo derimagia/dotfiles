a03b7c286d48bebae41e315be8fcfa50b5c75108
Dave Wikoff <dwikoff@mindgrub.com>
2017-02-07 19:37:34 -0500

    dsearch and trying new apps

--- plugins/php/dots/php.sh
+++ plugins/php/dots/php.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env zsh
 
-exit # Getting an error with php when we turn on file opcache for cli...
-
 [[ $OSTYPE =~ darwin ]] || exit 34
 
-
 local PHP_CONF_DIRS=($HOMEBREW_PREFIX/etc/php/7.*/conf.d(N))
-local OPCACHE_TMP_PATH=/tmp/php/opcache
 
-[[ -n $PHP_CONF_DIRS ]] || (echo 'PHP Config Directory not found.' && exit 1)
+[[ -n $PHP_CONF_DIRS ]] || (ink -c red 'PHP Config Directory not found.' && exit 1)
 
-mkdir -p $OPCACHE_TMP_PATH
-cat > $PHP_CONF_DIRS/zcustom-opcache.ini <<-INI
+cat > $PHP_CONF_DIRS/zcustom.ini <<-INI
 # Generated from Dot
-opcache.enable_cli=1
-opcache.file_cache=$OPCACHE_TMP_PATH
-opcache.file_cache_only=1
+[Date]
+date.timezone=America/New_York
 INI
-ink -c green "[PHP] Saved Opcache Config"
 
+ink -c green "[PHP] Saved Custom Config"
+
+# Getting an error with php when we turn on file opcache for cli...
+# local OPCACHE_TMP_PATH=/tmp/php/opcache
+# mkdir -p $OPCACHE_TMP_PATH
+#[Opcache]
+#opcache.enable_cli=1
+#opcache.file_cache=$OPCACHE_TMP_PATH
+#opcache.file_cache_only=1
