2bc32c1438b43d38c7e5a03c1889650e19fac496
Dave Wikoff <dwikoff@mindgrub.com>
2017-08-11 13:51:43 -0400

    Add container-transform

--- config/zsh/aliases.plugin.zsh
+++ config/zsh/aliases.plugin.zsh
@@ -65,6 +65,7 @@ alias dps='docker ps'
 alias di='docker images'
 alias dip='docker inspect --format "{{ .NetworkSettings.IPAddress }}"'
 alias docker-images-tree='docker run --rm -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images -t'
+alias container-transform='docker run --rm -v $(pwd):/data/ micahhausler/container-transform'
 
 # git
 alias g='git'
